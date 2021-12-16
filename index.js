4892b51318520599c1472170be005465e670701f
bmarv <beesemarvin@gmail.com>
2021-12-16 20:46:50 +0100

    bootstrap as frontend-engine embedded for client

--- index.js
+++ index.js
@@ -63,6 +63,19 @@ app.get('/client', function (req, res) {
   });
 });
 
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js", 
+  express.static(path.join(__dirname, "node_modules/jquery/dist"))
+);
+
 app.use(express.static(path.join(__dirname)));
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, 'views', 'pug-source')));
